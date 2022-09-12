128ef2dc8f081d18c1ff3b27cb36dd745f65fd0d
Vitor Guedes <vgm_rox@hotmail.com>
2022-09-12 11:36:12 -0300

    add changes required

--- queries.sql
+++ queries.sql
@@ -3,7 +3,7 @@
 -- MONDAY
 
 SELECT * FROM animals WHERE name LIKE '%mon';
-SELECT name FROM animals WHERE date_of_birth BETWEEN '2016-01-01' and '2019-01-01';
+SELECT name FROM animals WHERE date_of_birth BETWEEN '2016/01/01' and '2019/31/12';
 SELECT name FROM animals WHERE neutered = true AND escape_attempts < 3;
 SELECT date_of_birth FROM animals WHERE name IN ('Agumon', 'Pikachu');
 SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
